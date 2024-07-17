aa41e2f60246b8c3f7bee612b98fcba10b7435ee
“Antonio <antoniohl91@gmail.com>
2024-07-17 19:39:16 +0200

    Importamos var Configuracion a AppComponent

--- masterJS/aprendiendo-angular/src/app/app.component.ts
+++ masterJS/aprendiendo-angular/src/app/app.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+// Imports generados
+import { Configuracion } from './models/configuration';
+
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -10,5 +14,13 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'aprendiendo-angular';
+ public title = 'Master de JavaScript y Angular';
+ public descripcion: String;
+ public config;
+
+ constructor (){
+    this.title =  Configuracion.titulo;
+    this.descripcion = Configuracion.descripcion;
+    this.config = Configuracion;
+ }
 }
