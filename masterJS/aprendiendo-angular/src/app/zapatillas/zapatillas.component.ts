c5b91750bb7b6a8e6be716cb0b72131b61361de2
“Antonio <antoniohl91@gmail.com>
2024-07-17 19:19:26 +0200

    Creamos el componente zapatillas

---
+++ masterJS/aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-zapatillas',
+  standalone: true,
+  imports: [],
+  templateUrl: './zapatillas.component.html',
+  styleUrl: './zapatillas.component.css'
+})
+export class ZapatillasComponent {
+
+}
