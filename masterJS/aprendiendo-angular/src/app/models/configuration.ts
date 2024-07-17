96a3649ee7dbfeaa65a4f1b4515929fae407de47
“Antonio <antoniohl91@gmail.com>
2024-07-17 19:29:45 +0200

    Creamos carpeta models y variable Configuracion

---
+++ masterJS/aprendiendo-angular/src/app/models/configuration.ts
@@ -0,0 +1,7 @@
+export var Configuracion = {
+// Si añadimos export, podemos exportar esta variable al componente que queramos, puede ser variable, clase, lo que queramos.
+  color: "green",
+  fondo: "white",
+  titulo: "Aplicación con Angular",
+  descripcion: "Aprendiendo Angular"
+};
