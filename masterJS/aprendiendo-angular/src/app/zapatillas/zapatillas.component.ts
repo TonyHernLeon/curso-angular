9f8c61664aed90b4cabd2b359bd5128b9249e88b
“Antonio Hernandez <antoniohl91@gmail.com>
2024-07-23 19:42:31 +0200

    Creamos el modelo Zapatilla y lo implementamos en el component

--- masterJS/aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
+++ masterJS/aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { Zapatilla } from '../models/zapatillas';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-zapatillas',
+  selector: 'zapatillas',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './zapatillas.component.html',
   styleUrl: './zapatillas.component.css'
 })
 export class ZapatillasComponent {
 
+  public titulo: String = "Componente de zapatillas";
+  public zapatillas: Array<Zapatilla>;
+
+  constructor(){
+    this.zapatillas = [
+      new Zapatilla('Reebook Classic', 'Reebook', 'Blanco', 80, true),
+      new Zapatilla('Nike Runner MD', 'Nike', 'Negro', 60, true),
+      new Zapatilla('Adidas Yeezy', 'Adidas', 'gris', 180, false)
+    ];
+  }
+
+  ngOnInit(){
+    console.log(this.zapatillas);
+  }
 }
